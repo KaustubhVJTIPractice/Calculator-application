a94456962ba9a932f71b713be306104548ccd3a6
kaustubhchile <kaustubchile@gmail.com>
2026-01-04 21:20:58 +0530

    Add functionality

---
+++ calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def add(self, num1, num2):
+        return num1 + num2
+
+
+num1 = int(input("Enter the number 1:"))
+num2 = int(input("Enter the number 2:"))
+calci = Calculator()
+print(calci.add(num1, num2))
