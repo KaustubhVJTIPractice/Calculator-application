41dfe5201364b570db5f19ba9f83645e8428347c
kaustubhchile <kaustubchile@gmail.com>
2026-01-08 00:12:47 +0530

    Subtract functionality

--- calculator.py
+++ calculator.py
@@ -2,8 +2,12 @@ class Calculator:
     def add(self, num1, num2):
         return num2 + num1
 
+    def subtract(self, num3, num4):
+        return num3 - num4
+
 
 num1 = int(input("Enter the number 1:"))
 num2 = int(input("Enter the number 2:"))
 calci = Calculator()
 print(calci.add(num1, num2))
+print(calci.subtract(num1, num2))
