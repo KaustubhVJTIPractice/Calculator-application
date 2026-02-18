084561d0ce67283f7d0b9848e17c8714c1744ab2
kaustubhchile <72078555+kaustubhchile@users.noreply.github.com>
2026-02-18 16:08:23 +0530

    Add divide method to Calculator class

--- calculator.py
+++ calculator.py
@@ -8,6 +8,9 @@ class Calculator:
     def multiply(self, num5, num6):
         return num5*num6
 
+    def divide(self, num7, num8):
+        return num7/num8
+
 
 num1 = int(input("Enter the number 1:"))
 num2 = int(input("Enter the number 2:"))
@@ -15,6 +18,7 @@ calci = Calculator()
 print(calci.add(num1, num2))
 print(calci.subtract(num1, num2))
 print(calci.multiply(num1, num2))
+print(calci.divide(num1, num2))
 
 
 
