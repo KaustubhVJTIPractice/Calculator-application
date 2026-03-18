c341fcd32402fa440d535b95696ad65a8a809698
kaustubhchile <kaustubchile@gmail.com>
2026-03-19 01:19:35 +0530

    removing add

--- calculator.py
+++ calculator.py
@@ -1,7 +1,4 @@
 class Calculator:
-    def add(self, num1, num2):
-        return num2 + num1
-
     def subtract(self, num3, num4):
         return num3 - num4
 
@@ -12,6 +9,5 @@ class Calculator:
 num1 = int(input("Enter the number 1:"))
 num2 = int(input("Enter the number 2:"))
 calci = Calculator()
-print(calci.add(num1, num2))
 print(calci.subtract(num1, num2))
 print(calci.multiply(num1, num2))
